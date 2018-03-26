b6a07b6406376703c981bf37d2d5c212dba187ca
Caroline VG <caroline.vangossum@live.be>
2018-03-26 17:13:03 +0200

    routes/indexjs file edited -> works now

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,43 @@
 var express = require('express');
-var app = express.Router();
+var router = express.Router();
+
 var question, answer1, answer2;
+
 
 /* Bodyparser setup */
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
+router.use(bodyParser.urlencoded({
   extended: false
 }));
 
-/* use bodyparser */
-app.use(bodyParser.json());
+router.use(bodyParser.json());
 
-/* GET home page. */
-app.get('/', function(req, res, next) {
-  res.render('index', { answer2: a2, answer1: a1, question: q });
+router.get('/', function(req, res, next) {
+  res.render('index', {question: question, answer1: answer1, answer2: answer2 });
   
 });
 
+
 /* GET createpoll page */
-app.get('/createpoll', function(req, res, next){
-  res.render('live', {title: "Create Poll"});
+router.get('/createpoll', function(req, res, next){
+  res.render('createpoll', {title: "Create Poll"});
+  
 });
 
-/* POST createpoll page */
-app.post('/createpoll',function(req,res,next){
-  question = req.body.question;
-  answer1 = req.body.answer1;
-  answer2 = req.body.answer2;
+router.post('/createpoll',function(req,res,next){
+  a2 = req.body.answer2;
+  a1 = req.body.answer1;
+  q = req.body.question;
+  
+  
   res.redirect('/');
 });
+
+
+
+
+
+
+
+
+module.exports = router;
